80b23dc7b9d59f06007424794d1849cfd7014881
haru <haru.contact.me@gmail.com>
2021-02-12 11:41:08 +0900

    write url test code but have not finished

--- app/clans/tests/test_urls.py
+++ app/clans/tests/test_urls.py
@@ -1,15 +1,45 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
-from clans.views import home
+from . import views
+from accounts.models import User
+from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 
 
-class ClanUrlTests(TestCase):
+class ClanUrlRoutingTests(TestCase):
 
-    def test_url_resolves_to_home_view(self):
+    # テストユーザー作成方法
+    def setUp(self):
+        User.objects.create_user(
+            username='hogehoge',
+            name='ほげ',
+            age=12,
+        )
+
+    def test_url_resolves_to_home(self):
         found = resolve('')
-        self.assertEqual(found.func, home)
+        self.assertEqual(found.func, views.home)
 
-    def test_url_resolves_to_user_invite_notice_view(self):
-        found = resolve('')
+    def test_url_resolves_to_clan_create(self):
+        found = resolve('clan/create/')
+        self.assertEqual(found.func, views.clan_create)
 
+    def test_url_resolves_to_clan_list(self):
+        found = resolve('clan/list/')
+        self.assertEqual(found.func, views.clan_list)
+
+    # 詳細画面のテストコードの書き方
+    def test_url_resolves_to_clan_detail(self):
+        found = resolve('clan/<str:name>/')
+        self.assertEqual(found.func, views.clan_detail)
+
+
+
+class ClanHtmlTests(TestCase):
+
+    def test_home_html(self):
+        request = HttpRequest()
+        response = views.home(request)
+        expected_html = render_to_string('clans/home.html')
+        self.assertEqual(response.content.decode(), expected_html)
