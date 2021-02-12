e6e573b25554546f0b7cde8ea980c7b3feebb9e0
haru <haru.contact.me@gmail.com>
2021-02-12 12:18:21 +0900

    delete email field & method but have not checked the operation

--- app/accounts/models.py
+++ app/accounts/models.py
@@ -17,28 +17,28 @@ class CustomUserManager(UserManager):
     """ ユーザーマネージャー """
     use_in_migrations = True
 
-    def _create_user(self, email, password, **extra_fields):
-        if not email:
-            raise ValueError('The given email must be set')
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+    def _create_user(self, username, password, **extra_fields):
+        if not username:
+            raise ValueError('The given username must be set')
+        username = self.normalize_email(username)
+        user = self.model(username=username, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_user(self, email, password=None, **extra_fields):
+    def create_user(self, username, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_superuser', False)
-        return self._create_user(email, password, **extra_fields)
+        return self._create_user(username, password, **extra_fields)
 
-    def create_superuser(self, email, password, **extra_fields):
+    def create_superuser(self, username, password, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         if extra_fields.get('is_staff') is not True:
             raise ValueError('Superuser must have is_staff=True.')
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
-        return self._create_user(email, password, **extra_fields)
+        return self._create_user(username, password, **extra_fields)
 
 
 
@@ -105,10 +105,14 @@ class User(AbstractBaseUser, PermissionsMixin):
         verbose_name = _('user')
         verbose_name_plural = _('users')
 
-    def email_user(self, subject, message, from_email=None, **kwargs):
 
-        # このユーザーにメールを送信する
-        send_mail(subject, message, from_email, [self.email], **kwargs)
+
+    # def email_user(self, subject, message, from_email=None, **kwargs):
+
+    #     # このユーザーにメールを送信する
+    #     send_mail(subject, message, from_email, [self.email], **kwargs)
+
+
 
     @property
     def username(self):
