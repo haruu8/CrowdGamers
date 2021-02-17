bd570c93ee6bedd818f3f320fa4cb06406f89f56
haru <haru.contact.me@gmail.com>
2021-02-17 14:53:31 +0900

    create profile base view but have not finished make it

--- app/teams/views.py
+++ app/teams/views.py
@@ -32,41 +32,59 @@ home = HomeView.as_view()
 
 """ UserProfileに関するview """
 
-class UserDetailGameView(DetailView):
-    template_name = 'teams/accounts/account_detail_game.html'
+class UserProfileBaseView(DetailView):
+    template_name = 'teams/accounts/profile_extends_base.html'
     model = UserProfile
 
     def get_object(self):
-        username = self.kwargs.get("username")
+        username = self.kwargs.get('username')
         return get_object_or_404(User, username=username)
+
+
+
+class UserDetailGameView(UserProfileBaseView):
+    template_name = 'teams/accounts/account_detail_game.html'
+    model = UserProfile
+
+    # のちに修正
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx['game_title'] = UserProfile.objects.get(user=self.request.user)
+        return ctx
 
 account_detail_game = UserDetailGameView.as_view()
 
 
 
-class UserDetailFeatureView(DetailView):
+class UserDetailFeatureView(UserProfileBaseView):
     template_name = 'teams/accounts/account_detail_feature.html'
     model = UserProfile
 
-    def get_object(self):
-        username= self.kwargs.get("username")
-        return get_object_or_404(User, username=username)
+    # のちに修正
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx['feature'] = UserProfile.objects.get(user=self.request.user)
+        return ctx
 
 account_detail_feature = UserDetailFeatureView.as_view()
 
 
 
-class UserDetailDesiredJobTypeView(DetailView):
+class UserDetailDesiredJobTypeView(UserProfileBaseView):
     template_name = 'teams/accounts/account_detail_desired_job_type.html'
     model = UserProfile
 
-    def get_object(self):
-        username = self.kwargs.get("username")
-        return get_object_or_404(User, username=username)
+    # のちに修正
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx['desired'] = UserProfile.objects.get(user=self.request.user)
+        return ctx
 
 account_detail_desired_job_type = UserDetailDesiredJobTypeView.as_view()
 
 
+
+""" 通知関連 view """
 
 class UserInviteNotificationView(LoginRequiredMixin, OnlyYouMixin, TemplateView):
     template_name = 'teams/notification/user_invite_notification.html'
