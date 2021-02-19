3cf1d98c2548fbe645c367821eb4efd647ac7f0e
haru <haru.contact.me@gmail.com>
2021-02-19 11:03:57 +0900

    add img & add html title

--- app/teams/views/profile.py
+++ app/teams/views/profile.py
@@ -10,7 +10,6 @@ from accounts.models import User
 
 class UserProfileBaseView(DetailView):
     template_name = 'teams/accounts/profile_extends_base.html'
-    model = UserProfile
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
