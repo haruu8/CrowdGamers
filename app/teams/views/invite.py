2df6af369e962bf563e1cbe7a15b8be1cffbc203
haru <haru.contact.me@gmail.com>
2021-02-28 22:35:55 +0900

    Write the process in comments.

--- app/teams/views/invite.py
+++ app/teams/views/invite.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, FormView
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -40,11 +40,30 @@ class UserInviteCreateView(LoginRequiredMixin, CreateView):
     form_class = UserInviteCreateForm
     success_url = reverse_lazy('teams:home')
 
+    # from と to を設定
+    # オーナーしか送信できないようにする
     def form_valid(self, form):
         form.instance.user = self.request.user
-        return redirect(self.success_url)
+
+        """
+            * from user に自分を登録 (owner前提)
+            self.object.from_user = self.request.user
+
+            *** to_user にユーザーを保存 ***
+              * 送ろうとしているユーザーのオブジェクトを取得する
+              * object の to_user に 招待を送るユーザーを登録
+
+            invitation_user = User.objects.get(username=username)
+            self.object.to_user = invitation_user
+        """
+
+        result = super().form_valid(form)
+        return result
 
     def form_invalid(self, form):
         return render(self.request, '400.html', {'form': form})
+
+    def get_success_url(self):
+        return reverse('teams:account_detail_game', kwargs={'username': self.object.username})
 
 user_invite_create = UserInviteCreateView.as_view()
