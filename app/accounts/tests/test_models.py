1fe1be5a3da17fb19caf464ff26c61e52ae53021
haru <haru.contact.me@gmail.com>
2021-03-11 11:51:13 +0900

    Remove incomplete test code.

--- app/accounts/tests/test_models.py
+++ app/accounts/tests/test_models.py
@@ -3,19 +3,13 @@ from django.contrib.auth import get_user_model
 
 class UserTest(TestCase):
     def setUp(self):
-        self.model = get_user_model()
-        self.not_min_length_user = get_user_model().objects.create(
-            username='hog',
-        )
-        self.min_length_user = get_user_model().objects.create(
-            username='hoge',
-        )
+        pass
 
     def test_min_length(self):
         """
         最小文字数の境界値テスト
         """
-        self.assertEqual(self.min_length_user)
+        pass
 
     def test_max_length(self):
         """
