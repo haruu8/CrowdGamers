563fc5e821c3923e52b8a223ed03fed1c35548b3
haru <haru.contact.me@gmail.com>
2021-03-13 16:10:20 +0900

    change GetProfileView

--- app/teams/views/utils.py
+++ app/teams/views/utils.py
@@ -10,12 +10,12 @@ class GetProfileView(TemplateView):
     ヘッダー部分に表示するプロフィール情報を取得する
     """
     def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
         if self.request.user.is_anonymous:
             pass
         else:
-            context = super().get_context_data(**kwargs)
             context['user_profile'] = UserProfile.objects.get(user=self.request.user)
-            return context
+        return context
 
     def get_object(self):
         return get_object_or_404(User, username=self.kwargs.get('username'))
