896040f9e8e5a77f0f448408a4a59fcd3b9c012b
haru <haru.contact.me@gmail.com>
2021-03-13 14:12:25 +0900

    add docs, delete unnecessary test code

--- app/teams/tests/test_urls.py
+++ app/teams/tests/test_urls.py
@@ -1,49 +1,37 @@
 from django.test import TestCase
-from django.core.urlresolvers import resolve
-from . import views
-from accounts.models import User
+from django.urls import resolve, reverse
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+from teams import views
+from django.contrib.auth import get_user_model
 
 
-
-""" 正しい View が呼び出されているかテスト """
 
 class TeamUrlRoutingTests(TestCase):
+    """
+    正しい view が呼び出されているかテスト
 
-    # テストユーザー作成方法
-    def setUp(self):
-        User.objects.create_user(
-            username='hogehoge',
-            name='ほげ',
-            age=12,
-        )
+    Notes
+    -----
+    test_url_resolves_to_*** で関数定義している。 *** は views の名前
+    """
+    # def setUp(self):
+    #     get_user_model().objects.create_user(
+    #         username='hoge',
+    #     )
 
     def test_url_resolves_to_home(self):
         found = resolve('')
         self.assertEqual(found.func, views.home)
 
-    def test_url_resolves_to_team_create(self):
-        found = resolve('team/create/')
-        self.assertEqual(found.func, views.team_create)
-
-    def test_url_resolves_to_team_list(self):
-        found = resolve('team/list/')
-        self.assertEqual(found.func, views.team_list)
-
-    # 詳細画面のテストコードの書き方
-    def test_url_resolves_to_team_detail(self):
-        found = resolve('team/<str:team_name>/')
-        self.assertEqual(found.func, views.team_detail)
 
 
-
-""" 正しい HTML が返されているかテスト """
-
-class TeamHtmlTests(TestCase):
-
-    def test_home_html(self):
-        request = HttpRequest()
-        response = views.home(request)
-        expected_html = render_to_string('teams/home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+# class TeamHtmlTests(TestCase):
+#     """
+#     正しい html が表示されているかテスト
+#     """
+#     def test_home_html(self):
+#         request = HttpRequest()
+#         response = views.home(request)
+#         expected_html = render_to_string('teams/home.html')
+#         self.assertEqual(response.content.decode(), expected_html)
