4dcfb7276a1931ed9a5d2b38147ffb94d0884f34
haru <haru.contact.me@gmail.com>
2021-03-13 19:02:20 +0900

    add urls test

--- app/teams/tests/test_urls.py
+++ app/teams/tests/test_urls.py
@@ -2,8 +2,9 @@ from django.test import TestCase
 from django.urls import resolve, reverse
 from django.http import HttpRequest
 from django.template.loader import render_to_string
-from teams.views import *
 from django.contrib.auth import get_user_model
+from teams.models import Team, UserProfile
+from teams.views import *
 
 
 
@@ -35,7 +36,22 @@ class TeamStatusCodeTests(TestCase):
     test_***_status_code で関数定義している。 *** は urls に登録してある name
     """
     def setUp(self):
-        pass
+        self.team = Team.objects.create(
+            teamname='hoge',
+            name='hoge',
+            introduction='hoge',
+            desired_condition='hoge',
+            disclosed=True,
+        )
+        self.user = get_user_model().objects.create_user(
+            username='CrowdGamers',
+        )
+        self.user_profile = UserProfile.objects.update_or_create(
+            user=self.user,
+            team=self.team,
+            desired_condition='hoge',
+            disclosed=True,
+        )
 
     def test_home_status_code(self):
         url = reverse('teams:home')
@@ -76,6 +92,73 @@ class TeamStatusCodeTests(TestCase):
         url = reverse('teams:team_list')
         response = self.client.get(url)
         self.assertEquals(response.status_code, 200)
+
+    def test_team_update_status_code(self):
+        url = reverse('teams:team_update', args=[self.team])
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 403)
+
+    def test_team_delete_status_code(self):
+        url = reverse('teams:team_delete', args=[self.team])
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 403)
+
+    def test_team_detail_status_code(self):
+        url = reverse('teams:team_detail', args=[self.team])
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    # def test_team_detail_member_status_code(self):
+    #     url = reverse('teams:team_detail_member', args=[self.team])
+    #     response = self.client.get(url)
+    #     self.assertEquals(response.status_code, 200)
+
+    def test_team_detail_feature_status_code(self):
+        url = reverse('teams:team_detail_feature', args=[self.team])
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_team_detail_desired_condition_status_code(self):
+        url = reverse('teams:team_detail_desired_condition', args=[self.team])
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_team_member_add_status_code(self):
+        url = reverse('teams:team_member_add', args=[self.team])
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_team_member_delete_status_code(self):
+        url = reverse('teams:team_member_delete', kwargs={'teamname': self.team, 'username': self.user})
+        # url = reverse('teams:team_member_delete', args=[self.team])
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_account_detail_status_code(self):
+        url = reverse('teams:account_detail', args=[self.user])
+        # url = reverse('teams:team_member_delete', args=[self.team])
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_account_detail_feature_status_code(self):
+        url = reverse('teams:account_detail_feature', args=[self.user])
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_account_detail_desired_condition_status_code(self):
+        url = reverse('teams:account_detail_desired_condition', args=[self.user])
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_account_profile_update_status_code(self):
+        url = reverse('teams:account_profile_update', args=[self.user])
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 403)
+
+    def test_notification_status_code(self):
+        url = reverse('teams:notification', args=[self.user])
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 403)
 
 
 
